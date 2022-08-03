957a081a6990d6e4fa8d19dfd87fb1af40dfe942
ed-dinaoui <eddinaouibilal@gmail.com>
2022-08-03 15:41:43 -0400

    1 file modified, 1 added

--- src/pages/home/components/f-canvas/F_canvas.js
+++ src/pages/home/components/f-canvas/F_canvas.js
@@ -1,11 +1,60 @@
 import React, { useEffect, useState, memo } from 'react';
+import './F_canvas.css';
+var svg, cW, cH;
+
+function draw_pol() {
+  svg = document.getElementById('svg');
+  cW = parseFloat(getComputedStyle(svg).width);
+  cH = parseFloat(getComputedStyle(svg).height);
+
+  draw_in(cW, cH, 25, '1');
+  draw_in(cW / 1.5, cH / 3, -10, '2');
+  draw_in(cW / 40, cH / 3, -10, '3');
+}
+
+function draw_in(wid, hei, ky, con) {
+  let ocy = cH - ky;
+  var p_elem = document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'polygon'
+  );
+  let cordinat = `${cW / 2 - wid / 2},${ocy}  ${cW / 2},${cH - hei}  ${
+    cW / 2 + wid / 2
+  },${ocy} `;
+  p_elem.setAttributeNS(null, 'points', cordinat);
+  p_elem.classList.add('polygon_' + con);
+  svg.appendChild(p_elem);
+}
 
 function Home_canvas() {
+  useEffect(() => {
+    draw_pol();
+  });
   return (
     <div id="center-S">
-      <svg viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg">
-        <polygon points="0,100 50,25 50,75 100,0" fill="white" />
-      </svg>
+      <div class="svg_con">
+        <svg
+          id="svg"
+          viewBox="0 0 65vh 60vh"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient id="b_gr_2" gradientTransform="rotate(90)">
+              <stop offset="5%" stop-color="rgb(1,1,1)" />
+              <stop offset="95%" stop-color="#006eff" />
+            </linearGradient>
+            <linearGradient id="b_gr" gradientTransform="rotate(90)">
+              <stop offset="10%" stop-color="#006eff" />
+              <stop offset="90%" stop-color="black" />
+            </linearGradient>
+            <radialGradient id="rad_gra" fr="40%" cy="130%">
+              <stop offset="10%" stop-color="white" />
+              <stop offset="40%" stop-color="#006eff" />
+              <stop offset="90%" stop-color="black" />
+            </radialGradient>
+          </defs>
+        </svg>
+      </div>
     </div>
   );
 }
