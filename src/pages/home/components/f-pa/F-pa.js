e65cb43cbac0b121358d9b12d6d1d259ea4fa61d
ed-dinaoui <eddinaouibilal@gmail.com>
2022-08-12 12:44:50 -0400

    4 files modified, 1 deleted

--- src/pages/home/components/f-pa/F-pa.js
+++ src/pages/home/components/f-pa/F-pa.js
@@ -1,75 +1,75 @@
-import React , {useEffect , memo , useRef} from 'react';
+import React, { useEffect, memo, useRef } from 'react';
 import './F-pa.css';
 import { Home_logo } from '../logo/Logo';
-import { isVisible } from '../../home' ;
+import { isVisible } from '../../home';
 
-var container , cursor , wid , hei , m_size ;
+var container, cursor, wid, hei, m_size;
 
-const rando_m = (min,max) =>{
-  return min + Math.random() * ( max - min )
+const rando_m = (min, max) => {
+  return min + Math.random() * (max - min);
+};
+function handle_move(e) {
+  cursor.style.left = e.pageX - 200 + 'px';
+  cursor.style.top = e.pageY - 200 + 'px';
 }
-function handle_move(e){
-  cursor.style.left = (e.pageX - 200) + 'px' ;
-  cursor.style.top = (e.pageY - 200) + 'px' ;
-}
-function draw_stars(){
-  container = document.getElementById('cont') ;
-  cursor = document.getElementById('cursor') ;
-  wid = parseFloat(getComputedStyle(container).width) ;
-  hei = parseFloat(getComputedStyle(container).height) ;
+function draw_stars() {
+  container = document.getElementById('cont');
+  cursor = document.getElementById('cursor');
+  wid = parseFloat(getComputedStyle(container).width);
+  hei = parseFloat(getComputedStyle(container).height);
 
-  draw_in() ;
+  draw_in();
 }
-function draw_in(){
-  m_size = (wid * hei) / 7000 ;
-  for (let i = 0 ; i < m_size ; i++){
+function draw_in() {
+  m_size = (wid * hei) / 7000;
+  for (let i = 0; i < m_size; i++) {
     let star = document.createElement('div');
-    
-    let o_size = rando_m(1 , 4) ;
-    star.style.position = 'relative' ;
-    star.style.top = rando_m(0 , 100 - ( m_size / 6 )) + "%" ;
-    star.style.left = rando_m(0 , 100) + "%" ;
-    star.style.width = o_size + 'px' ;
-    star.style.height = o_size + 'px' ;
-    star.classList.add('a_star') ;
-    
+
+    let o_size = rando_m(1, 4);
+    star.style.top = rando_m(0, 100 - m_size / 6) + '%';
+    star.style.left = rando_m(0, 100) + '%';
+    star.style.width = o_size + 'px';
+    star.style.height = o_size + 'px';
+    star.classList.add('a_star');
+
     star.animate(
       [
-        {backgroundColor : `rgba(255,255,255,${rando_m(.6,.8)})`} ,
-        {backgroundColor : `rgba(255,255,255,${rando_m(.0,.2)})`}
-      ],{
-        duration : rando_m(2000,8000) ,
-        easing : 'linear' ,
-        iterations : Infinity , 
-        direction : 'alternate'
-      })
-    
-    container.appendChild(star) ;
+        { backgroundColor: `rgba(255,255,255,${rando_m(0.6, 0.8)})` },
+        { backgroundColor: `rgba(255,255,255,${rando_m(0.0, 0.2)})` },
+      ],
+      {
+        duration: rando_m(2000, 8000),
+        easing: 'linear',
+        iterations: Infinity,
+        direction: 'alternate',
+      }
+    );
+
+    container.appendChild(star);
   }
-  document.body.addEventListener('mousemove',handle_move)
+  document.body.addEventListener('mousemove', handle_move);
 }
-function undraw_stars(){
-  $('.a_star').remove() ;
-  document.body.removeEventListener('mousemove',handle_move) ;
+function undraw_stars() {
+  $('.a_star').remove();
+  document.body.removeEventListener('mousemove', handle_move);
 }
 function FPa(props) {
-  useEffect(()=>{
+  useEffect(() => {
     //
     return () => {
-      undraw_stars()   
-    }
-  },[])
+      undraw_stars();
+    };
+  }, []);
   return (
     <div id="center-S" className="f-Pa">
-      <Home_logo >
+      <Home_logo>
         <h1 ref={props.aRef}>Hello</h1>
       </Home_logo>
-      {isVisible(props.aRef) ? draw_stars() : undraw_stars() }
-      <div id='cursor' ></div>
-      <div id='cont'></div>
+      {isVisible(props.aRef) ? draw_stars() : undraw_stars()}
+      <div id="cursor"></div>
+      <div id="cont"></div>
     </div>
   );
 }
 
-
-export default memo(FPa) ;
+export default memo(FPa);
