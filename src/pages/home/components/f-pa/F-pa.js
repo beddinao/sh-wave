d44fdcf8cee93c61354e72ee13687b61678ce058
ed-dinaoui <eddinaouibilal@gmail.com>
2022-08-12 22:12:25 -0400

    2 files modified

--- src/pages/home/components/f-pa/F-pa.js
+++ src/pages/home/components/f-pa/F-pa.js
@@ -3,14 +3,13 @@ import './F-pa.css';
 import { Home_logo } from '../logo/Logo';
 import { isVisible } from '../../home';
 
-var container, cursor, wid, hei, m_size;
-
+var container, cursor, wid, hei, m_size  ;
 const rando_m = (min, max) => {
   return min + Math.random() * (max - min);
 };
 function handle_move(e) {
-  cursor.style.left = e.pageX - 200 + 'px';
-  cursor.style.top = e.pageY - 200 + 'px';
+  cursor.style.left = e.pageX - 100 + 'px';
+  cursor.style.top = e.pageY - 100 + 'px';
 }
 function draw_stars() {
   container = document.getElementById('cont');
@@ -21,34 +20,34 @@ function draw_stars() {
   draw_in();
 }
 function draw_in() {
-  m_size = (wid * hei) / 7000;
+  m_size = (wid * hei) / 20000;
   for (let i = 0; i < m_size; i++) {
     let star = document.createElement('div');
-
     let o_size = rando_m(1, 4);
+    let o_co = `rgba(255,255,255,${rando_m(.01,.8)})` ;
+
     star.style.top = rando_m(0, 100 - m_size / 6) + '%';
     star.style.left = rando_m(0, 100) + '%';
     star.style.width = o_size + 'px';
     star.style.height = o_size + 'px';
+    star.style.backgroundColor = o_co ;
+    star.style.boxShadow = `0 0 ${o_size + 5}px `+ o_co ;
     star.classList.add('a_star');
-
     star.animate(
       [
-        { backgroundColor: `rgba(255,255,255,${rando_m(0.6, 0.8)})` },
-        { backgroundColor: `rgba(255,255,255,${rando_m(0.0, 0.2)})` },
-      ],
-      {
-        duration: rando_m(2000, 8000),
-        easing: 'linear',
-        iterations: Infinity,
-        direction: 'alternate',
-      }
-    );
-
+        { opacity : 0.5  } ,
+        { opacity : 1  }
+      ],{
+        duration : rando_m(1000,5000) ,
+        easing : 'linear' ,
+        iterations : Infinity , 
+        direction : 'alternate'
+      })
     container.appendChild(star);
   }
   document.body.addEventListener('mousemove', handle_move);
 }
+
 function undraw_stars() {
   $('.a_star').remove();
   document.body.removeEventListener('mousemove', handle_move);
@@ -66,10 +65,12 @@ function FPa(props) {
         <h1 ref={props.aRef}>Hello</h1>
       </Home_logo>
       {isVisible(props.aRef) ? draw_stars() : undraw_stars()}
+      <div id="cont" >
+      </div>
       <div id="cursor"></div>
-      <div id="cont"></div>
     </div>
   );
 }
 
 export default memo(FPa);
+
