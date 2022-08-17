ff005d28316226b8fbdc299fdbf1377abd050f39
ed-dinaoui <eddinaouibilal@gmail.com>
2022-08-17 19:08:32 -0400

    1 file modified

--- src/pages/home/components/welcome/Welcom.js
+++ src/pages/home/components/welcome/Welcom.js
@@ -5,7 +5,7 @@ import $ from 'jquery';
 import Home_sides from '../sides/Sides';
 import Solar_h1 from '../so_h1/So_h1';
 import w_1 from './img/wp_3.jpg' ;
-import w_2 from './img/wp_1.jpg' ;
+import w_2 from './img/wp_2.jpeg' ;
 
 var a_nom, b_nom, a_width ,whi;
 const tar = {
